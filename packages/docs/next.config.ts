1728af264e1e0f5e7b170308724a1031c55e9f72
Oliwier Perzyński <olek@oliverperzyk.com>
2026-07-16 13:21:39 +0200

    refactor(config): refactored configuration of PostCSS and Next.js; adjusted files affected by Prettier in root repository

--- packages/docs/next.config.ts
+++ packages/docs/next.config.ts
@@ -1,8 +1,34 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
-
-export default nextConfig;
+/**
+ * @summary Configuration of Next.js.
+ * @description This file is used to configure the Next.js project.
+ * @see {@link https://nextjs.org/docs/app/api-reference/config/next-config-js}
+ */
+export default {
+    typedRoutes: true,
+    reactCompiler: true,
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/repository",
+                destination: "https://github.com/oliverperzyk/chest-ui",
+                permanent: true,
+                priority: true,
+            },
+            {
+                source: "/issues",
+                destination: "https://github.com/oliverperzyk/chest-ui/issues",
+                permanent: true,
+                priority: true,
+            },
+            {
+                source: "/github",
+                destination: "https://github.com/oliverperzyk/chest-ui",
+                permanent: true,
+                priority: true,
+            },
+        ]
+    },
+} satisfies NextConfig
