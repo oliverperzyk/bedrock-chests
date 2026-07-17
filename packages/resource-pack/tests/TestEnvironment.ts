615a1c7c61db4d2dd626417b9c93d57f09ba6490
Oliwier Perzyński <olek@oliverperzyk.com>
2026-07-17 15:48:38 +0300

    feat(rp): added testing environment; minor adjustments for library's typing libraries

---
+++ packages/resource-pack/tests/TestEnvironment.ts
@@ -0,0 +1,26 @@
+/**
+ * @summary Test environment for the library.
+ * @description A class that initializes the test environment for the library.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+class TestEnvironment {
+    /**
+     * @summary Private constructor.
+     * @description Private constructor to prevent instantiation and inheritance of the class.
+     */
+    private constructor() {}
+
+    /**
+     * @summary Static initializer.
+     * @description Static initializer to initialize the test environment.
+     */
+    static {
+        void this.init()
+    }
+
+    /**
+     * @summary Initializes the test environment.
+     * @description Initializes the test environment.
+     */
+    private static async init(): Promise<void> {}
+}
