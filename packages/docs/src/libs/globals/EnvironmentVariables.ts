83b996f6ad78f0729e7c365a8410345d8bd165bb
Oliwier Perzyński <olek@oliverperzyk.com>
2026-07-17 17:50:58 +0300

    feat(env->manager): added parser for environment variables

--- packages/docs/src/libs/globals/EnvironmentVariables.ts
+++ packages/docs/src/libs/globals/EnvironmentVariables.ts
@@ -1,3 +1,5 @@
+import { EnvironmentVariablesDataManager } from "./managers/security/EnvironmentVariablesDataManager"
+
 /**
  * @summary Class to read environment variables.
  * @description This class stores all environment variables in read-only properties.
@@ -13,13 +15,27 @@ class EnvironmentVariables {
      * @summary Environment that application runs in.
      * @description Application's environment is in demand to enable or disable some of the things on the website.
      */
-    public static readonly NODE_ENV: string = "TEST"
+    public static readonly NODE_ENV: string = EnvironmentVariablesDataManager.getNodeEnvironment()
 
     /**
      * @summary Endpoint to the website. Used mostly for SEO things.
      * @description Endpoint to the website, that allows to configure e.g. robots, sitemap to point to actual endpoints, improving website's SEO.
      */
-    public static readonly WEBSITE_ENDPOINT_URL: URL = new URL("https://oliverperzyk.com")
+    public static readonly WEBSITE_ENDPOINT_URL: URL = EnvironmentVariablesDataManager.getURL(
+        "NEXT_PUBLIC_WEBSITE_ENDPOINT_URL",
+        process.env.NEXT_PUBLIC_WEBSITE_ENDPOINT_URL,
+        true,
+    )
+
+    /**
+     * @summary Static block.
+     * @description This static block adds additional checks on environment variables.
+     */
+    static {
+        // We do not care about the value here, we just need to know that it's a valid value
+        // or at least it's not provided.
+        EnvironmentVariablesDataManager.getPort("APP_PORT", process.env.APP_PORT, false)
+    }
 }
 
 export { EnvironmentVariables }
---
+++ packages/docs/src/libs/models/general/environment/enums/NodeEnvironment.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * @summary Enumeration of node environments.
+ * @description This enumeration is used to identify the environment that the application runs in.
+ */
+const enum NodeEnvironment {
+    /**
+     * @summary Production environment.
+     * @description This environment is used for production deployments.
+     */
+    PRODUCTION = "PRODUCTION",
+    /**
+     * @summary Development environment.
+     * @description This environment is used for development purposes.
+     */
+    DEVELOPMENT = "DEVELOPMENT",
+    /**
+     * @summary Test environment.
+     * @description This environment is used for testing the application in e.g. CI workflow.
+     */
+    TEST = "TEST",
+}
+
+export { NodeEnvironment }
