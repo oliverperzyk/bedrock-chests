6fa55d5fa8088827bec4c66295a86a58c58c793f
Oliwier Perzyński <olek@oliverperzyk.com>
2026-07-17 20:23:42 +0300

    refactor(layout): improved viewport information & defined basic styles

--- packages/docs/src/app/layout.tsx
+++ packages/docs/src/app/layout.tsx
@@ -2,20 +2,40 @@ import type { Metadata, Viewport } from "next"
 import type { IChildren } from "../libs/models/components/general/interfaces/IChildren"
 import type { JSX } from "react"
 import { ThemeProvider } from "@wrksz/themes/next"
-import { Geist } from "next/font/google"
-// import "./globals.css";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-})
+import { SocialData } from "../libs/globals/SocialData"
+import "@/oliverperzyk/styles/GlobalStyles.css"
 
 const metadata: Metadata = {}
-const viewport: Viewport = {}
 
+/**
+ * @summary Viewport information.
+ * @description Default viewport information for the whole application.
+ */
+const viewport: Viewport = {
+    width: "device-width",
+    colorScheme: "light dark",
+    initialScale: 1,
+    themeColor: [
+        {
+            media: "(prefers-color-scheme: dark)",
+            color: SocialData.THEME_COLOR.dark,
+        },
+        {
+            media: "(prefers-color-scheme: light)",
+            color: SocialData.THEME_COLOR.light,
+        },
+    ],
+}
+
+/**
+ * @summary Global layout of the website.
+ * @description This layout defines default structure of the website.
+ * @param children - Destructred property that contains content of routes.
+ * @returns Default structure of the website.
+ */
 function RootLayout({ children }: Readonly<IChildren>): JSX.Element {
     return (
-        <html lang="en-US" className={`${geistSans.variable} h-full antialiased`}>
+        <html lang="en-US" className="h-full antialiased" suppressHydrationWarning>
             <body className="min-h-full flex flex-col">
                 <ThemeProvider>{children}</ThemeProvider>
             </body>
---
+++ packages/docs/src/libs/globals/SocialData.ts
@@ -0,0 +1,12 @@
+class SocialData {
+    private constructor() {}
+
+    public static readonly BACKGROUND_COLOR: string = "#"
+
+    public static readonly THEME_COLOR = {
+        light: "#ffffff",
+        dark: "#000000",
+    }
+}
+
+export { SocialData }
