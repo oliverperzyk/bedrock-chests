2c7a7a4ee635c2bc4340dfcc71de54af12694543
Oliwier Perzyński <olek@oliverperzyk.com>
2026-07-17 16:22:47 +0300

    feat(root->lint): added ESLint for root directory

---
+++ eslint.config.ts
@@ -0,0 +1,38 @@
+import json from "@eslint/json"
+import markdown from "@eslint/markdown"
+import { defineConfig } from "eslint/config"
+
+/**
+ * @summary ESLint configuration.
+ * @description Configuration of the root repository of library.
+ * @see {@link https://eslint.org/docs/latest/use/configure/configuration-files}
+ */
+export default defineConfig([
+    {
+        ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+    },
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.jsonc"],
+        plugins: { json },
+        language: "json/jsonc",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.json5"],
+        plugins: { json },
+        language: "json/json5",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.md"],
+        plugins: { markdown },
+        language: "markdown/gfm",
+        extends: ["markdown/recommended"],
+    },
+])
